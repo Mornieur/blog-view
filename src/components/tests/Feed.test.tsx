40423e4b50bbdeb255c2b908a784438de6f60529
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 14:51:55 -0300

    test: changing unit tests of feed component

--- src/components/tests/Feed.test.tsx
+++ src/components/tests/Feed.test.tsx
@@ -1,38 +1,73 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Feed from '../Feed';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { BlogPost } from '../../services/useGetBlogPosts';
 
-describe('Feed Component', () => {
+jest.mock('../components/PostItem', () => {
+  return ({ post }: { post: BlogPost }) => (
+    <div data-testid={`post-item-${post.id}`}>{post.title}</div>
+  );
+});
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
+describe('Feed', () => {
   const mockPosts = [
     { id: 1, title: 'Test Post 1', body: 'Body of test post 1', userId: 1 },
     { id: 2, title: 'Test Post 2', body: 'Body of test post 2', userId: 2 },
   ];
 
-  it('renders the Feed component', () => {
+  it('displays loading skeleton when loading', () => {
     render(
-      <Feed
-        searchQuery=""
-        isLoading={false}
-        isError={false}
-        posts={mockPosts}
-      />
+      <Router>
+        <Feed searchQuery="" isLoading={true} isError={false} posts={[]} />
+      </Router>
     );
-
-    expect(screen.getByText('Test Post 1')).toBeInTheDocument();
-    expect(screen.getByText('Test Post 2')).toBeInTheDocument();
+    expect(screen.queryByTestId('post-list')).toBeNull();
   });
 
-  it('displays loading skeleton when isLoading is true', () => {
-    render(<Feed searchQuery="" isLoading={true} isError={false} posts={[]} />);
-
-    expect(screen.getByTestId('loading-skeleton')).toBeInTheDocument();
-  });
-
-  it('displays error message when isError is true', () => {
-    render(<Feed searchQuery="" isLoading={false} isError={true} posts={[]} />);
-
+  it('displays an error message when there is an error', () => {
+    render(
+      <Router>
+        <Feed searchQuery="" isLoading={false} isError={true} posts={[]} />
+      </Router>
+    );
     expect(
-      screen.getByText('An error occurred while fetching posts.')
+      screen.getByText('Nenhuma postagem encontrada.')
     ).toBeInTheDocument();
+  });
+
+  it('displays posts when provided', () => {
+    render(
+      <Router>
+        <Feed
+          searchQuery=""
+          isLoading={false}
+          isError={false}
+          posts={mockPosts}
+        />
+      </Router>
+    );
+    expect(screen.getByTestId('post-item-1')).toHaveTextContent('First Post');
+    expect(screen.getByTestId('post-item-2')).toHaveTextContent('Second Post');
+  });
+
+  it('filters posts based on search query', () => {
+    render(
+      <Router>
+        <Feed
+          searchQuery="Second"
+          isLoading={false}
+          isError={false}
+          posts={mockPosts}
+        />
+      </Router>
+    );
+    expect(screen.queryByText('First Post')).toBeNull();
+    expect(screen.getByTestId('post-item-2')).toHaveTextContent('Second Post');
   });
 });
