fa993468e8457a3d9710afa21b7e7fe425e30416
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 13:21:54 -0300

    perf: adding another type of screen

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -1,6 +1,11 @@
 import { useMediaQuery, useTheme } from '@mui/material';
 
-export const useIsMobile = () => {
+export const useIsBreakpointDown = (
+  breakpoint: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+) => {
   const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down('md'));
+  return useMediaQuery(theme.breakpoints.down(breakpoint));
 };
+
+export const useIsMobile = () => useIsBreakpointDown('md');
+export const useIsTablet = () => useIsBreakpointDown('lg');
