fad75fd7981f8555f4f5682e5e560e7d37d1bcca
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 13:19:11 -0300

    test: small changing unit tests

--- src/components/tests/Feed.test.tsx
+++ src/components/tests/Feed.test.tsx
@@ -1,97 +1,38 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { useBlogPosts } from '../../services/useGetBlogPosts';
-import { MemoryRouter } from 'react-router-dom';
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import Feed from '../Feed';
 
-jest.mock('../../services/useGetBlogPosts', () => ({
-  useBlogPosts: jest.fn(),
-}));
-
-const mockSetPage = jest.fn();
-
-const mockPosts = [
-  { id: '1', title: 'Test Post 1', body: 'Body of test post 1' },
-  { id: '2', title: 'Test Post 2', body: 'Body of test post 2' },
-];
-
-class MockIntersectionObserver {
-  callback: (entries: any[]) => void;
-  isIntersecting: boolean;
-
-  constructor(callback: (entries: any[]) => void) {
-    this.callback = callback;
-    this.isIntersecting = false;
-  }
-
-  observe = jest.fn((entries: IntersectionObserverEntry[] | undefined) => {
-    if (entries && entries.length > 0 && entries[0].isIntersecting) {
-      this.isIntersecting = true;
-    }
-    this.callback(entries || [{ isIntersecting: this.isIntersecting }]);
-  });
-
-  disconnect = jest.fn(() => {
-    this.isIntersecting = false;
-  });
-}
-
-// Store the original implementation
-const originalIntersectionObserver = global.IntersectionObserver;
-
-beforeAll(() => {
-  global.IntersectionObserver = MockIntersectionObserver as any;
-});
-
-afterAll(() => {
-  global.IntersectionObserver = originalIntersectionObserver;
-});
-
 describe('Feed Component', () => {
-  beforeEach(() => {
-    (useBlogPosts as jest.Mock).mockReturnValue({
-      allInfoBlog: mockPosts,
-      isLoadingBlog: false,
-      isErrorBlog: false,
-      hasMoreBlog: true,
-      setPage: jest.fn(),
-    });
+  const mockPosts = [
+    { id: 1, title: 'Test Post 1', body: 'Body of test post 1', userId: 1 },
+    { id: 2, title: 'Test Post 2', body: 'Body of test post 2', userId: 2 },
+  ];
+
+  it('renders the Feed component', () => {
+    render(
+      <Feed
+        searchQuery=""
+        isLoading={false}
+        isError={false}
+        posts={mockPosts}
+      />
+    );
+
+    expect(screen.getByText('Test Post 1')).toBeInTheDocument();
+    expect(screen.getByText('Test Post 2')).toBeInTheDocument();
   });
 
-  it('renders skeleton loaders when data is being fetched', () => {
-    (useBlogPosts as jest.Mock).mockReturnValue({
-      allInfoBlog: [],
-      isLoadingBlog: true,
-      isErrorBlog: false,
-      hasMoreBlog: true,
-      setPage: jest.fn(),
-    });
+  it('displays loading skeleton when isLoading is true', () => {
+    render(<Feed searchQuery="" isLoading={true} isError={false} posts={[]} />);
 
-    render(<Feed searchQuery="" />, { wrapper: MemoryRouter });
-    expect(screen.getAllByTestId('loading-box')).toHaveLength(1);
+    expect(screen.getByTestId('loading-skeleton')).toBeInTheDocument();
   });
 
-  it('displays error message on error state', () => {
-    (useBlogPosts as jest.Mock).mockReturnValue({
-      allInfoBlog: [],
-      isLoadingBlog: false,
-      isErrorBlog: true,
-      hasMoreBlog: true,
-      setPage: jest.fn(),
-    });
+  it('displays error message when isError is true', () => {
+    render(<Feed searchQuery="" isLoading={false} isError={true} posts={[]} />);
 
-    render(<Feed searchQuery="" />, { wrapper: MemoryRouter });
     expect(
-      screen.getByText('Ocorreu um erro. Tente novamente mais tarde.')
+      screen.getByText('An error occurred while fetching posts.')
     ).toBeInTheDocument();
   });
-
-  it('handles click on "Read More" button', async () => {
-    render(<Feed searchQuery="" />, { wrapper: MemoryRouter });
-    const readMoreButton = screen.getByTestId('read-more-button-1');
-    fireEvent.click(readMoreButton);
-    expect(useBlogPosts().setPage).toHaveBeenCalledTimes(0);
-  });
 });
-
-// rever teste aqui no Read Mode Button deveria receber o fireEvent e ir para (1) e naio ficar em 0
