bf5532042f43a19ba356a721afc3b90f83477fca
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 10:11:55 -0300

    feat: creating unit tests for the suggestions component

---
+++ src/components/tests/SuggestedBox.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SuggestedBox from '../SuggestedBox';
+
+describe('SuggestedBox Component', () => {
+  test('renders SuggestedBox component with title and suggestions', () => {
+    render(<SuggestedBox />);
+
+    const titleElement = screen.getByText('Suggested for you');
+    expect(titleElement).toBeInTheDocument();
+
+    const suggestions = screen.getAllByText(/Suggestion/);
+    expect(suggestions).toHaveLength(6);
+
+    suggestions.forEach((suggestion, index) => {
+      expect(suggestion).toHaveTextContent(`Suggestion ${index + 1}`);
+    });
+  });
+});
