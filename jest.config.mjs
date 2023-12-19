6c455bbd620801bc53e8a7f2c699f9044b30fb0f
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 09:51:58 -0300

    build: create a jest config

---
+++ jest.config.mjs
@@ -0,0 +1,24 @@
+const createJestConfig = require('jest-create-config');
+
+/** @type {import('jest').Config} */
+const config = createJestConfig({
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+
+  transformOptions: {
+    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+    plugins: [
+      'babel-plugin-transform-react-jsx-source',
+      'babel-plugin-transform-typescript-metadata',
+    ],
+  },
+
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
+  coverageReporters: ['json', 'lcov', 'text-summary'],
+});
+
+module.exports = config;
