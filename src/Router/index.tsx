31a8ee5ded0e48794bb7e6c8b7f3bf5c5fc58313
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 18:35:43 -0300

    feat: add error page in the routes

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Post from '../pages/Post';
 import Home from '../pages/Home';
+import Error404 from '../pages/404';
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
