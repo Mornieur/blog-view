e26c30f5f3167d933a1ab4688bb76ee9834f332a
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 09:54:41 -0300

    feat: add GlobalStyles and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import { GlobalStyles } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppRouter from './Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyles
+      styles={{
+        margin: 0,
+        padding: 0,
+        backgroundColor: '#ececec',
+      }}
+    />
+    <AppRouter />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
