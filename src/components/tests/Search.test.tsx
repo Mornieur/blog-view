8e51e57dd9e86e61bf9c37d5e1ac6577bbb0446f
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 18:34:56 -0300

    test: changing placeholder to English

--- src/components/tests/Search.test.tsx
+++ src/components/tests/Search.test.tsx
@@ -17,23 +17,19 @@ describe('SearchBar Component', () => {
   });
 
   test('renders SearchBar component', () => {
-    expect(screen.getByPlaceholderText('Pesquisar...')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Search...')).toBeInTheDocument();
     expect(screen.getByLabelText('search')).toBeInTheDocument();
   });
 
   test('updates input value and calls setSearchQuery on change', () => {
-    const input = screen.getByPlaceholderText(
-      'Pesquisar...'
-    ) as HTMLInputElement;
+    const input = screen.getByPlaceholderText('Search...') as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'test query' } });
     expect(input.value).toBe('test query');
     expect(mockSetSearchQuery).toHaveBeenCalledWith('test query');
   });
 
   test('submits form and calls setSearchQuery', () => {
-    const input = screen.getByPlaceholderText(
-      'Pesquisar...'
-    ) as HTMLInputElement;
+    const input = screen.getByPlaceholderText('Search...') as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'test submit' } });
     fireEvent.submit(screen.getByRole('search'));
     expect(mockSetSearchQuery).toHaveBeenCalledWith('test submit');
