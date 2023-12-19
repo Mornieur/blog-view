3a6cc20d9e50614e4c04f60e3965586a3e70757b
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-19 10:09:17 -0300

    test: creating unit tests for the header component

---
+++ src/components/tests/Header.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../Header';
+import { useIsMobile } from '../../hooks/useIsMobile';
+
+jest.mock('../../hooks/useIsMobile', () => ({
+  useIsMobile: jest.fn(),
+}));
+jest.mock('../MenuComponent', () => {
+  return {
+    __esModule: true,
+    default: (props: { handleMenuClose: () => void }) => (
+      <div data-testid="mock-menu">
+        Mock Menu
+        <button onClick={props.handleMenuClose}>Close Menu</button>
+      </div>
+    ),
+  };
+});
+
+describe('Header Component', () => {
+  const mockSetSearchQuery = jest.fn();
+
+  beforeEach(() => {
+    // Default mock for useIsMobile hook
+    (useIsMobile as jest.Mock).mockReturnValue(false);
+  });
+
+  test('renders Header component', () => {
+    render(<Header setSearchQuery={mockSetSearchQuery} />);
+    expect(screen.getByTestId('mock-menu')).toBeInTheDocument();
+    expect(screen.getByText('My Blog')).toBeInTheDocument();
+    expect(screen.getByLabelText('open drawer')).toBeInTheDocument();
+  });
+
+  test('opens menu on button click', () => {
+    render(<Header setSearchQuery={mockSetSearchQuery} />);
+    fireEvent.click(screen.getByLabelText('open drawer'));
+    expect(screen.getByText('Mock Menu')).toBeInTheDocument();
+  });
+
+  test('closes menu on close button click', () => {
+    render(<Header setSearchQuery={mockSetSearchQuery} />);
+    fireEvent.click(screen.getByLabelText('open drawer'));
+    fireEvent.click(screen.getByText('Close Menu'));
+  });
+
+  test('renders different layout for mobile', () => {
+    (useIsMobile as jest.Mock).mockReturnValue(true);
+    render(<Header setSearchQuery={mockSetSearchQuery} />);
+  });
+});
