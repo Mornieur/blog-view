a2dc689e4068eefaebb3ccd20786616881dcad81
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-20 14:23:32 -0300

    build: add vite settings

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ command, mode }) => {
+  const buildConfig = {
+    plugins: [react(), viteTsconfigPaths()],
+    esbuild: {
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+    },
+    build: {
+      outDir: 'build',
+      hunkSizeWarningLimit: 1000 * 500,
+      rollupOptions: {
+        output: {
+          inlineDynamicImports: true,
+        },
+      },
+    },
+  };
+
+  const devConfig = { ...buildConfig, define: { global: {} } };
+
+  if (command === 'serve') return devConfig;
+  return buildConfig;
+});
