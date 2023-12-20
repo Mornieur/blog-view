415b24608421ad218c11776eb240cc9f2430d4ee
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-20 14:57:30 -0300

    feat: changing component import type

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import { GlobalStyles } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRouter from './router';
-
+import AppRouter from './router/index';
 import { globalStyles } from './theme';
 
 const root = ReactDOM.createRoot(
