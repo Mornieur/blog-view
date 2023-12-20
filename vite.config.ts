187a8ed28dcf68417b1582e59d73881de53f57a4
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-20 16:08:13 -0300

    build: changing vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,15 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ command, mode }) => {
-  const buildConfig = {
-    plugins: [react(), viteTsconfigPaths()],
-    esbuild: {
-      jsxFactory: 'React.createElement',
-      jsxFragment: 'React.Fragment',
-    },
-    test: {
-      environment: 'jsdom',
-      globals: true,
-    },
-    build: {
-      outDir: 'build',
-      hunkSizeWarningLimit: 1000 * 500,
-      rollupOptions: {
-        output: {
-          inlineDynamicImports: true,
-        },
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
       },
     },
-  };
-
-  const devConfig = { ...buildConfig, define: { global: {} } };
-
-  if (command === 'serve') return devConfig;
-  return buildConfig;
+  },
 });
