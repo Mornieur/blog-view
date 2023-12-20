c45d4bb1d6a4adc981919fa0493c04958a9e0b59
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-20 12:01:34 -0300

    style: add style theme into GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,14 @@ import { GlobalStyles } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './router';
+import { globalStyles } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <>
-    <GlobalStyles
-      styles={{
-        margin: 0,
-        padding: 0,
-        backgroundColor: '#ececec',
-      }}
-    />
+    <GlobalStyles styles={globalStyles} />
     <AppRouter />
   </>
 );
