aa5a5d024174d55bf795fe0f125d756853aa5d43
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-12-20 12:07:51 -0300

    style: changing error page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import { Box, Typography, Button, Container } from '@mui/material';
+import { Box, Typography, Button, SxProps, Theme } from '@mui/material';
 
-const boxErrorStyles = {
+const boxErrorStyles: SxProps<Theme> = {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+  flexDirection: 'column',
   height: '100vh',
   width: '100vw',
-  backgroundColor: '#000',
+  backgroundColor: '#6e3296',
   overflow: 'hidden',
   p: 0,
   borderRadius: 0,
 };
 
-const containerErrorStyles = {
-  maxWidth: 700,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  borderRadius: '10px',
-  backgroundColor: '#fff',
-  p: 5,
+const buttonStyles: SxProps<Theme> = {
+  mt: 2,
+  backgroundColor: '#f98404',
+  color: '#fff',
+  fontWeight: 'bold',
+};
+
+const errorIconStyles: SxProps<Theme> = {
+  fontSize: 50,
+  color: '#f98404',
+  p: 1,
 };
 
 const Error404: React.FC = () => {
@@ -36,24 +39,30 @@ const Error404: React.FC = () => {
       boxShadow={3}
       sx={boxErrorStyles}
     >
-      <Container maxWidth="sm" sx={containerErrorStyles}>
-        <ErrorOutlineIcon sx={{ fontSize: 50, color: 'orange' }} />
-        <Typography variant="h4" component="h1" color="error" gutterBottom>
-          Error 404
-        </Typography>
-        <Typography variant="body1" color="textSecondary">
-          The page you're looking for doesn't exist or has been moved.
-        </Typography>
-        <Button
-          component={RouterLink}
-          to="/"
-          variant="contained"
-          sx={{ mt: 2, backgroundColor: '#6e3296' }}
-          role="button"
-        >
-          Go Home
-        </Button>
-      </Container>
+      <ErrorOutlineIcon sx={errorIconStyles} />
+      <Typography variant="h4" component="h1" color="white" gutterBottom>
+        Error 404
+      </Typography>
+      <img
+        src={require('../assets/error-404.png')}
+        alt="Error 404"
+        style={{
+          maxWidth: '100%',
+          objectFit: 'contain',
+        }}
+      />
+      <Typography variant="h6" sx={{ p: 1 }} color="white">
+        The page you're looking for doesn't exist or has been moved.
+      </Typography>
+      <Button
+        component={RouterLink}
+        to="/"
+        variant="contained"
+        sx={buttonStyles}
+        role="button"
+      >
+        Go Home
+      </Button>
     </Box>
   );
 };
